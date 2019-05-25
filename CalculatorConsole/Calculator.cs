ee329e88ad736a93f5da6cf023ef73c64da151a8
Heiko Vetter <mail@heikovetter.com>
2019-05-25 22:36:11 +0200

    add source files

---
+++ CalculatorConsole/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorConsole
+{
+    public class Calculator
+    {
+        public double Addition(double value1, double value2) => value1 + value2;
+    }
+}
---
+++ CalculatorConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            Console.WriteLine("Enter two numbers!");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var numbers = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var result = calculator.Addition(Convert.ToDouble(numbers[0]), Convert.ToDouble(numbers[1]));
+                Console.WriteLine("Result: " + result);
+            }
+        }
+    }
+}
